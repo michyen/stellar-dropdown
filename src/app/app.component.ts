a1b5825c1cc02936838116b149bac00508df2477
michyen <michyen@gmail.com>
2019-03-18 17:09:33 -0700

    cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,4 @@
-import {
-  Component,
-  HostListener,
-  ElementRef,
-  EventEmitter,
-  Output
-} from "@angular/core";
+import { Component } from "@angular/core";
 
 @Component({
   selector: "my-app",
@@ -12,15 +6,5 @@ import {
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  // @Output() outsideClick = new EventEmitter();
-  // @HostListener("document:click", ["$event.target"])
-  // public onClick(targetElement) {
-  //   const clickedInside = this.elementRef.nativeElement.contains(targetElement);
-  //   if (!clickedInside) {
-  //     // hide dropdown
-  //     this.outsideClick.emit();
-  //   }
-  // }
-
-  constructor(private elementRef: ElementRef) {}
+  constructor() {}
 }
